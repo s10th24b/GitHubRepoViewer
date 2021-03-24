d584bf72e01ef8eac46675243f41206f6cbfc8d3
s10th24b <allen246@naver.com>
2021-03-25 03:40:54 +0900

    Initial Commit

---
+++ app/src/main/java/kr/s10th24b/app/githubrepoviewer/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.s10th24b.app.githubrepoviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
