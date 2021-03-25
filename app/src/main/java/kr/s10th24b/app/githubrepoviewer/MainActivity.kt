a204c2cc03080e07b449236100d9ebe7cffd3baa
s10th24b <allen246@naver.com>
2021-03-25 22:50:40 +0900

    complete main activity and add listactivity

--- app/src/main/java/kr/s10th24b/app/githubrepoviewer/MainActivity.kt
+++ app/src/main/java/kr/s10th24b/app/githubrepoviewer/MainActivity.kt
@@ -2,10 +2,15 @@ package kr.s10th24b.app.githubrepoviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        githubImageView.setOnClickListener {
+            Toast.makeText(applicationContext,"Welcome to GithubRepoViewer",Toast.LENGTH_SHORT).show()
+        }
     }
 }
